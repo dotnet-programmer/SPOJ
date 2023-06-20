1fd1b2f117764a8405a8aa501837cf6e795b98a0
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-06-20 12:58:32 +0200

    SPOJ 42 Adding Reversed Numbers

--- SPOJ.Lib/Classical.cs
+++ SPOJ.Lib/Classical.cs
@@ -2,11 +2,34 @@
 
 public static class Classical
 {
+	// 1 Life, the Universe, and Everything
 	public static void TEST_Life_the_Universe_and_Everything()
 	{
 		while (int.TryParse(Console.ReadLine(), out int x) && x != 42)
 		{
 			Console.WriteLine(x);
+		}
+	}
+
+	// 42 Adding Reversed Numbers
+	public static void ADDREV_Adding_Reversed_Numbers()
+	{
+		int numberOfTests = int.Parse(Console.ReadLine());
+		for (int i = 0; i < numberOfTests; i++)
+		{
+			var input = Console.ReadLine().Split(' ');
+			int number1 = int.Parse(RemoveZeros(string.Join("", input[0].Reverse())));
+			int number2 = int.Parse(RemoveZeros(string.Join("", input[1].Reverse())));
+			Console.WriteLine(RemoveZeros(string.Join("", (number1 + number2).ToString().Reverse())));
+		}
+		static string RemoveZeros(string value)
+		{
+			int i = 0;
+			while (value[i] == '0')
+			{
+				i++;
+			}
+			return i == 0 ? value : value[i..];
 		}
 	}
 }
