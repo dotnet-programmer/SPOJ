44cd7cd8306a2fa802b09945cc3bf4309e514b8e
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-06-29 08:54:25 +0200

    SPOJ 2598 Kabalistyczny zapis daty - JZAPKAB

--- SPOJ.Lib/PolishEasy.cs
+++ SPOJ.Lib/PolishEasy.cs
@@ -1905,6 +1905,19 @@ public static class PolishEasy
 		}
 	}
 
+	// 2598 Kabalistyczny zapis daty - https://pl.spoj.com/problems/JZAPKAB/
+	public static void JZAPKAB_Kabalistyczny_zapis_daty()
+	{
+		Dictionary<char, int> letters = new() { ['a'] = 1, ['b'] = 2, ['c'] = 3, ['d'] = 4, ['e'] = 5, ['f'] = 6, ['g'] = 7, ['h'] = 8, ['i'] = 9, ['k'] = 10, ['l'] = 20, ['m'] = 30, ['n'] = 40, ['o'] = 50, ['p'] = 60, ['q'] = 70, ['r'] = 80, ['s'] = 90, ['t'] = 100, ['v'] = 200, ['x'] = 300, ['y'] = 400, ['z'] = 500, };
+		int sum = 0;
+		string input = Console.ReadLine();
+		foreach (var item in input)
+		{
+			sum += letters[item];
+		}
+		Console.WriteLine(sum);
+	}
+
 	// 3366 Sprawdzanie sudoku - https://pl.spoj.com/problems/SUDOKUC/
 	public static void SUDOKUC_Sprawdzanie_sudoku()
 	{
